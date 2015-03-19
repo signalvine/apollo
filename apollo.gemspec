08bdfc4a7a251725f8f1dc2d778b346f0e48a052
Brendan Tobolaski <brendan@tobolaski.com>
2015-03-19 12:31:29 -0400

    Add methods for interacting with rmq

--- apollo.gemspec
+++ apollo.gemspec
@@ -25,4 +25,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.8"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency 'minitest', '~> 5.5.1'
+
+  spec.add_dependency 'rabbitmq_manager', '~> 0.3.0'
 end
--- lib/apollo.rb
+++ lib/apollo.rb
@@ -1,5 +1,6 @@
 require "apollo/version"
 require "yaml"
+require 'rabbitmq_manager'
 
 module Apollo
   class Cluster
@@ -24,14 +25,59 @@ module Apollo
       @hosts[host]
     end
 
+    # Connects to the rabbitmq admin port on the specified host and waits until the specified queue has no messags
+    # opts - The hash of options (default: {})
+    #   :sleep_duration - the time to wait between checking the queue (default: 1)
+    #   :timeout - the maximum amount of time before giving up (default: nil)
+    #   :vhost - the rabbitmq vhost that the queue is on (default: '/')
+    def wait_for_queue_drain(host, queue, opts = {})
+      raise "host #{host} not configured in the inventory" if @hosts[host].nil?
+
+      sleep_duration = opts.fetch(:sleep_duration, 1)
+      timeout = opts.fetch(:timeout, nil)
+
+      start = Time.now
+      while check_queue_length(host, queue, opts) != 0
+        if not timeout.nil? and (Time.now.to_f - start.to_f) > timeout
+          raise 'wait_for_queue_drain exceeded timeout'
+        end
+        sleep sleep_duration
+      end
+    end
+
+    # Connects to the rabbitmq admin port on the specified host and gets the number of messages waiting in the specified
+    # queue
+    # opts - The hash of options (default: {})
+    #   :vhost - the rabbitmq vhost that the queue is on (default: '/')
+    def check_queue_length(host, queue, opts={})
+      host = @hosts[host]
+      vhost = opts.fetch(:vhost, '/')
+      username = CGI.escape host.fetch('rmq_username', 'guest')
+      password = CGI.escape host.fetch('rmq_password', 'guest')
+      port = host.fetch('rmq_port', 15672)
+
+      manager = RabbitMQManager.new "http://#{username}:#{password}@#{address host}:#{port}"
+      manager.queue(vhost, queue)['messages']
+    end
+
     private
 
     def process_host_list(host_list)
       list = {}
       host_list.each do |key, value|
+        raise "host #{key} is not addressable" if value['ip'].nil? and value['hostname'].nil?
+        raise "host #{key} has an invalid rmq_port #{value['rmq_port']}" unless value['rmq_port'].nil? or value['rmq_port'].is_a? Numeric
         list[key.to_sym] = value
       end
       list
+    end
+
+    def address(host)
+      unless host['ip'].nil?
+        host['ip']
+      else
+        host['hostname']
+      end
     end
   end
 end
